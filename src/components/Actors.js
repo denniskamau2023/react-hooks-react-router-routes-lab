50b2cb632c5e2d9bc479f2be768cf0439f0c1aeb
Your Name <youremail@domain.com>
2023-05-08 11:14:02 +0300

    done

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,23 @@
 import React from "react";
 import { actors } from "../data";
+import { Link } from "react-router-dom";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const act = actors;
+  return (
+    <div>
+      {act.map((acting) => {
+        return (
+          <ul key={acting.id}>
+            <li>
+              <Link to="/actors"> </Link>
+              {acting}
+            </li>
+          </ul>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Actors;
